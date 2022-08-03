1f0dc85fa381a5b60b17a6f98ac3ecb417b394cf
BaffIrodov <sergej.davidyuk@yandex.ru>
2022-08-04 01:03:25 +0300

    errors table init

--- src/app/components/errors/errors.component.ts
+++ src/app/components/errors/errors.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Errors} from "../../domain/errors";
 
 @Component({
   selector: 'app-errors',
@@ -9,7 +10,21 @@ export class ErrorsComponent implements OnInit {
 
   constructor() { }
 
+  results: Errors[] = [];
+
+  cols: any[];
+
   ngOnInit(): void {
+    this.columnsConstruct();
   }
 
+  columnsConstruct() {
+    this.cols = [
+      { field: 'classAndLine', header: 'Класс и строка' },
+      { field: 'descriptionError', header: 'Описание ошибки' },
+      { field: 'verificationError', header: 'Обработано' },
+      { field: 'payload', header: 'Информация' },
+      { field: 'dateTime', header: 'Дата ошибки' },
+    ];
+  }
 }
---
+++ src/app/domain/errors.ts
@@ -0,0 +1,7 @@
+export class Errors {
+  classAndLine: string;
+  descriptionError: string;
+  verificationError: boolean;
+  payload: string;
+  dateTime: Date;
+}
